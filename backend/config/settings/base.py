7b3a5bec7634647d142713d714166c3abe1f1ec7
Taofick <mdtaofick45418@gmail.com>
2026-08-04 19:42:40 +0600

    Complete authentication module with JWT

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -18,12 +18,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Third-party
+
+    # Third Party
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
-    # Project apps
+    "drf_spectacular",
+
+    # Project Apps
     "common",
     "apps.accounts",
     "apps.courses",
@@ -101,6 +104,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
@@ -125,3 +129,10 @@ CORS_ALLOWED_ORIGINS = config(
     default="http://localhost:5173",
     cast=Csv(),
 )
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "EduCore LMS API",
+    "DESCRIPTION": "Production Ready Learning Management System API",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
